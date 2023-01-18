94ace916eeb34ff6090fabdab637d8cd2c5f6e18
Andrei Makarov <andrei@kiskolabs.com>
2023-01-18 17:42:31 +0200

    Remove async from parser

--- src/parser.ts
+++ src/parser.ts
@@ -2,6 +2,6 @@ import { tokenizeHTML } from 'prettier-html-templates';
 import { expressionTypeMatcher } from './expression_type_matcher';
 
 export default {
-  parse: async (text: string) => tokenizeHTML(text, /<%[\s\S]*?%>/gm, expressionTypeMatcher),
+  parse: (text: string) => tokenizeHTML(text, /<%[\s\S]*?%>/gm, expressionTypeMatcher),
   astFormat: 'erb-ast',
 };
